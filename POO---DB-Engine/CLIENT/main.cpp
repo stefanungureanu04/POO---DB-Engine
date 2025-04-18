503f9e9dfa3dcb49eb2ba48af2ef620dfd63dedd
Stefan Ungureanu <stefan.ungureanu@stud.mta.ro>
2025-04-18 19:32:31 +0300

    .

--- POO---DB-Engine/CLIENT/main.cpp
+++ POO---DB-Engine/CLIENT/main.cpp
@@ -4,9 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << "hello from client";
-    //QApplication a(argc, argv);
-    //CLIENT w;
-    //w.show();
-    //return a.exec();
+    QApplication a(argc, argv);
+    CLIENT w;
+    w.show();
+    return a.exec();
 }
