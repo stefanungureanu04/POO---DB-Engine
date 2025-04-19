d2cc6a037bf1b2abb2e3b3f01717c3e6b5f67e82
Stefan Ungureanu <stefan.ungureanu@stud.mta.ro>
2025-04-19 03:04:40 +0300

    .

--- POO---DB-Engine/SERVER/main.cpp
+++ POO---DB-Engine/SERVER/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "SocketLib.h"
 
 int main()
 {
+	SocketException exception("test", 100);
+
+	std::cout << exception;
+
 	std::cout << "hello from server";
 	return 0;
 }
