012133158c2c03883b62fb881ebf9e773400e4ea
ADI <ADI@DESKTOP-1LL1FVU>
2025-04-22 22:38:43 +0300

    Am adaugat fisierul .ui pentru optiuni.

--- POO---DB-Engine/CLIENT/EnvironmentWindow.cpp
+++ POO---DB-Engine/CLIENT/EnvironmentWindow.cpp
@@ -1,6 +1,7 @@
 #include "EnvironmentWindow.h"
 #include "ui_EnvironmentWindow.h"
 
+
 EnvironmentWindow::EnvironmentWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::EnvironmentWindow)
